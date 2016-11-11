5ea6504fc957afa9a2045242cb51120014670cf9
Michael Spackman <MSPACKMA@1800contacts.com>
2016-11-11 11:10:38 -0700

    use service for product list

--- APM-Start/app/app.component.ts
+++ APM-Start/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from './products/product.service';
 
 @Component({
     moduleId: module.id,
@@ -8,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
             <h1>{{pageTitle}}</h1>
             <pm-products></pm-products>
         </div>
-    `
+    `,
+    providers: [ProductService]
 })
 export class AppComponent implements OnInit {
     constructor() { }
--- APM-Start/app/shared/star.component.ts
+++ APM-Start/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnChanges, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     moduleId: module.id,
@@ -10,8 +10,13 @@ export class StarComponent implements OnInit, OnChanges {
     constructor() { }
     @Input() rating: number;
     starWidth: number;
+    @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
+
     ngOnInit() { }
     ngOnChanges(): void {
         this.starWidth = this.rating * 86 / 5;
+    }
+    onClick(): void {
+        this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
     }
 }
